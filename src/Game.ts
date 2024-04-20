92acbd9717bb6555690c25f2bb2fc25544ac8f0f
Ricardo <ricarditomontserrat@gmail.com>
2024-04-20 23:21:17 +0200

    Added base methods to create for everything and some new types

--- src/Game.ts
+++ src/Game.ts
@@ -1,4 +1,4 @@
-import { Bodies, Body, Bounds, Composite, Engine, Runner, type IChamferableBodyDefinition } from 'matter-js';
+import { Bodies, Body, Composite, Engine, Runner, type IChamferableBodyDefinition } from 'matter-js';
 import P5 from 'p5';
 
 type Position = { x: number, y: number };
@@ -13,11 +13,11 @@ type PlayerStick = { position: Position, size: Size, color: Color, angle: Angle 
 type Hole = { position: Position, size: Size, balls: Ball[] };
 type Table = { position: Position, size: Size, color: Color, holes: Hole[] };
 
-type Game = { table: Table, playerSticks: PlayerStick[], balls: Ball[]};
+type PlayerId = number;
+type GameState = unknown;
+type Game = { table: Table, players: PlayerStick[], playerTurn: PlayerId, balls: Ball[], state: GameState };
 
 let engine = Engine.create();
-let boxes: Body[] = [];
-let toRemove: number[] = [];
 
 function Box(x: number, y: number, w: number, h: number, options?: IChamferableBodyDefinition): Body {
     let body = Bodies.rectangle(x, y, w, h, options);
@@ -25,41 +25,27 @@ function Box(x: number, y: number, w: number, h: number, options?: IChamferableB
     return body;
 }
 
-function BoxSize(bounds: Bounds): { w: number, h: number } {
-    const { min, max } = bounds;
-    return { w: max.x - min.x, h: max.y - min.y };
-}
-
+let game: Game;
 export default (p: P5) => {
-    p.mouseDragged = () => {
-        boxes.push(Box(p.mouseX, p.mouseY, 50, 50));
-    }
     p.setup = () => {
         p.createCanvas(800, 640);
-        p.background(0);
-
-        boxes.push(Box(p.width / 2, p.height - 20, p.width, 20, { isStatic: true }));
-        Runner.run(engine); // Runs the physics engine on the background
+        let balls = createBalls(p);
+        let table = createTable(p);
+        let players = createPlayers(p, 2);
+        game = startGame({ table, balls, players });
+        Runner.run(engine);
+        p.loop();
     };
     p.draw = () => {
         p.background(27);
-        boxes.forEach((box, i) => {
-            if (box.position.y > p.height || box.position.x < 0 || box.position.x > p.width) {
-                Composite.remove(engine.world, box);
-                toRemove.push(i);
+        if (turnEnded(game)) {
+            if (winner(game)) {
+                p.noLoop();
+                showWinner(p, game);
+                return;
             }
-            const { w, h } = BoxSize(box.bounds);
-            p.push();
-            // set x, y to the center of the rectangle since matter.js uses
-            // the center of but p5.js uses the top-left corner
-            p.rectMode(p.CENTER);
-            p.translate(box.position.x, box.position.y);
-            p.rotate(box.angle);
-            p.rect(0, 0, w, h);
-            p.pop();
-        });
-
-        toRemove.forEach(i => { boxes.splice(i, 1); });
-        toRemove.length = 0;
+            nextTurn(game);
+        }
+        draw(p, game);
     };
 };
